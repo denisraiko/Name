79b031276d9542c32f06203b42a23bcb185ad5b9
Denis Raiko <denisraiko@MacBook-Air-Denis.local>
2025-01-20 20:04:37 +0300

    Проект внесен в репозиторий

---
+++ Name/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Name
+//
+//  Created by Denis Raiko on 20.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
